e584c40a8893802ab71170d1e095aefce66e0252
Taylam Moura <tay.moura@hotmail.com>
2025-12-01 18:41:35 -0300

    refactor(auth): alteração @NotEmpty para @NotBlank e remoção do construtor do DTO

--- ms-auth/src/main/java/com/reading/ms_auth/dto/UserLoginDTO.java
+++ ms-auth/src/main/java/com/reading/ms_auth/dto/UserLoginDTO.java
@@ -2,19 +2,18 @@ package com.reading.ms_auth.dto;
 
 import com.reading.ms_auth.entity.User;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
 public record UserLoginDTO(
-        @NotEmpty(message = "O email é obrigatório!")
+        @NotBlank(message = "O email é obrigatório!")
         @Email(message = "Escreva um email válido!")
         String email,
 
-        @NotEmpty(message = "Digite sua password.")
+        @NotBlank(message = "Digite sua password.")
         @Size(min = 8, message = "A password deve ter no mínimo 8 números")
         String password) {
 
-    public UserLoginDTO(User user){
-        this(user.getEmail(), user.getPassword());
-    }
+
 }
