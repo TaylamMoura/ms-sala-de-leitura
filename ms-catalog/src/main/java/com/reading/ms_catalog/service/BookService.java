093cc173b270ea28a4b8defe2ff6613c4638d676
Taylam Moura <tay.moura@hotmail.com>
2025-12-22 19:46:55 -0300

    refactor(catalog): usar userId em vez de Usuario e ajustar métodos de CRUD de BookService.java

--- ms-catalog/src/main/java/com/reading/ms_catalog/service/BookService.java
+++ ms-catalog/src/main/java/com/reading/ms_catalog/service/BookService.java
@@ -1,15 +1,14 @@
 package com.reading.ms_catalog.service;
 
-
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.reading.ms_catalog.entity.Book;
 import com.reading.ms_catalog.repository.BookRepository;
 import com.reading.ms_catalog.dto.BookDTO;
-import com.reading.sala_de_leitura.entity.Usuario;
 import jakarta.validation.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +19,8 @@ import java.util.List;
 @Service
 public class BookService {
 
+    private static final String BASE_URL = "https://www.googleapis.com/books/v1/volumes?q=";
+
     private final APIConnection apiConnection;
     private final BookRepository repository;
 
@@ -29,24 +30,29 @@ public class BookService {
         this.repository = repository;
     }
 
-    public BookDTO searchByTitle(String title) {
+    public BookDTO searchByTitle(String bookTitle) {
         try {
-            String titleEncoded = URLEncoder.encode("intitle:" + title, StandardCharsets.UTF_8.toString());
+            String titleEncoded = URLEncoder.encode("intitle:" + bookTitle, StandardCharsets.UTF_8.toString());
             String jsonResponse = apiConnection.booksJson(titleEncoded);
 
             JsonObject jsonObject = JsonParser.parseString(jsonResponse).getAsJsonObject();
             JsonArray items = jsonObject.getAsJsonArray("items");
 
             if (items != null && !items.isEmpty()) {
-                JsonObject volumeInfo = items.get(0).getAsJsonObject().getAsJsonObject("volumeInfo");
+                JsonObject item = items.get(0).getAsJsonObject();
+
+                JsonObject volumeInfo = item.getAsJsonObject("volumeInfo");
+                JsonObject saleInfo = item.getAsJsonObject("saleInfo");
+
                 String title = volumeInfo.get("title").getAsString();
                 String author = volumeInfo.getAsJsonArray("authors").get(0).getAsString();
                 int pageCount = volumeInfo.get("pageCount").getAsInt();
                 String thumbnail = volumeInfo.getAsJsonObject("imageLinks").get("thumbnail").getAsString();
                 String publishedDate = volumeInfo.get("publishedDate").getAsString();
-                int ano = publishedDate != null ? Integer.parseInt(publishedDate.split("-")[0]) : 0;
+                int year = publishedDate != null ? Integer.parseInt(publishedDate.split("-")[0]) : 0;
+                String country = saleInfo != null && saleInfo.has("country") ? saleInfo.get("country").getAsString() : "N/A";
 
-                return new BookDTO(null, title, author, pageCount, thumbnail, ano, 0, false);
+                return new BookDTO(null, title, author, pageCount, thumbnail, year, false, country);
 
             } else {
                 System.out.println("Livro não encontrado. Tente outro título!");
@@ -60,7 +66,7 @@ public class BookService {
 
 
     @Transactional
-    public void saveBook(BookDTO bookDTO, Usuario usuarioLogado){
+    public void saveBook(BookDTO bookDTO, Long userId){
 
         String urlCapa = bookDTO.coverUrl();
 
@@ -75,47 +81,43 @@ public class BookService {
                 bookDTO.pages(),
                 urlCapa,
                 bookDTO.publicationYear(),
-                usuarioLogado,
-                false
+                userId,
+                false,
+                bookDTO.country()
         );
 
         repository.save(book);
     }
 
 
-    public List<BookDTO> listSavedBooks(Usuario usuario){
-        List<Book> books = repository.findByUsuario(usuario);
+    public List<BookDTO> listSavedBooks(Long userId){
+        List<Book> books = repository.findByUserId(userId);
         return books.stream().map(BookDTO::new).toList();
     }
 
 
     @Transactional
-    public void deleteBook(Long id, Usuario usuario){
-        Book book = repository.findById(id)
+    public void deleteBook(Long id, Long userId){
+        Book book = repository.findByIdAndUserId(id, userId)
                 .orElseThrow(() -> new ValidationException("Livro não escontrado"));
 
-        if (!book.getUserId().equals(usuario)){
-            throw new ValidationException("Você não tem permissão para excluir este book.");
-        }
         repository.delete(book);
     }
 
 
     @Transactional
-    public Book updateBook(Long id, BookUpdate bookUpdate, Usuario usuario) {
-        Book book = repository.findById(id)
+    public Book updateBook(Long id, BookUpdate bookUpdate, Long userId) {
+        Book book = repository.findByIdAndUserId(id, userId)
                 .orElseThrow(() -> new ValidationException("Livro não escontrado"));
-        if (!book.getUserId().equals(book.getUserId())){
-            throw new ValidationException("Você não tem permissão para excluir este book.");
-        }
+
         book.update(bookUpdate);
         repository.save(book);
         return book;
     }
 
 
-    public BookDTO getBookDetails(Long id, Usuario usuario) {
-        Book book = repository.findByIdAndUsuario(id, usuario)
+    public BookDTO getBookDetails(Long id, Long userId) {
+        Book book = repository.findByIdAndUserId(id, userId)
                 .orElseThrow(()-> new ValidationException("Livro não encontrado"));
         return  new BookDTO(book);
     }
