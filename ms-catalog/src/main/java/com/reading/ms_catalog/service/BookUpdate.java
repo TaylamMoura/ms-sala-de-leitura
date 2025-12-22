aa35efb77926958801c97bcd3a3e2ef0a911eac7
Taylam Moura <tay.moura@hotmail.com>
2025-12-22 19:48:25 -0300

    refactor(catalog): adicionar atributos title, author, country ao dto BookUpdate

--- ms-catalog/src/main/java/com/reading/ms_catalog/service/BookUpdate.java
+++ ms-catalog/src/main/java/com/reading/ms_catalog/service/BookUpdate.java
@@ -2,6 +2,9 @@ package com.reading.ms_catalog.service;
 
 public record BookUpdate(
         Long id,
+        String title,
+        String author,
+        String country,
         int pages,
         int publicationYear) {
 }
