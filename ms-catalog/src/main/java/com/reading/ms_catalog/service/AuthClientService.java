d4e7500e55afab472d5d48dff01447f6ea744948
Taylam Moura <tay.moura@hotmail.com>
2025-12-22 19:30:17 -0300

    fix(catalog):corrigir método validateToken para corrigir erro de validação

--- ms-catalog/src/main/java/com/reading/ms_catalog/service/AuthClientService.java
+++ ms-catalog/src/main/java/com/reading/ms_catalog/service/AuthClientService.java
@@ -1,59 +1,47 @@
 package com.reading.ms_catalog.service;
 
-import com.reading.ms_catalog.dto.UserIdResponse;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.beans.factory.annotation.Value;
 
-import java.util.Map;
-
-//Serviço para se comunicar com outro Microsserviço de autenticação
+//Serviço para se comunicar com o Microsserviço de autenticação
 
 @Service
 public class AuthClientService {
 
-    private final String authServiceName;
-    private final String authServicePath;
+    private final String authServiceName = "ms-auth";
     private final RestTemplate restTemplate;
 
-    public AuthClientService(RestTemplate restTemplate,
-                             @Value("${spring.application.name}") String authServiceName,
-                             @Value("${auth.service.path}") String authServicePath) {
+    public AuthClientService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
-        this.authServiceName = authServiceName;
-        this.authServicePath = authServicePath;
     }
 
-    // Método que recebe um token JWT e consulta o MS-Auth para descobrir o ID do usuário
 
-    public Long buscarUsuarioId(String tokenJWT) {
+    //Valida Token no ms-auth e retorna o userID
+    public Long validateToken(String token) {
+        String endpoint = "http://ms-auth/usuarios/validate-token";
+
         HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", tokenJWT);
-        HttpEntity<String> entity = new HttpEntity<>(headers);
+        headers.set("Authorization", "Bearer " + token);
 
-        String endpoint = "http://" + authServiceName + authServicePath + "/user-id";
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
 
         try {
-            ResponseEntity<UserIdResponse> response = restTemplate.exchange(
+            ResponseEntity<Long> response = restTemplate.exchange(
                     endpoint,
                     HttpMethod.GET,
                     entity,
-                    UserIdResponse.class
+                    Long.class
             );
 
-            if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
-                return  response.getBody().userId();
-            }
-
+            return response.getBody();
 
         } catch (Exception e) {
-            System.err.println("Falha ao se comunicar com MS-Auth: " + e.getMessage());
-            throw new RuntimeException("Erro de comunicação com serviço de autenticação.");
+            throw new RuntimeException("Falha ao validar token no MS-Auth: " + e.getMessage());
         }
-        throw new RuntimeException("ID de usuário não encontrado ou autenticação inválida.");
     }
 }
+
