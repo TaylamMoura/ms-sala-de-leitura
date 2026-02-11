59e4ab082e6847f6d2217861af81b56bf55b2e84
Taylam Moura <tay.moura@hotmail.com>
2026-02-11 18:40:13 -0300

    fix(gateway): corrigir SecurityConfig do ms-gateway para validar JWT com ServerBearerTokenAuthenticationConverter e desabilitar formLogin/httpBasic

--- ms-gateway/src/main/java/com/reading/ms_gateway/security/SecurityConfig.java
+++ ms-gateway/src/main/java/com/reading/ms_gateway/security/SecurityConfig.java
@@ -1,12 +1,12 @@
 package com.reading.ms_gateway.security;
 
 import com.reading.ms_gateway.filter.JwtReactiveAuthenticationManager;
-import com.reading.ms_gateway.filter.JwtServerAuthenticationConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.web.server.authentication.ServerBearerTokenAuthenticationConverter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
 
@@ -16,13 +16,15 @@ public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http,
-                                                            JwtReactiveAuthenticationManager authManager,
-                                                            JwtServerAuthenticationConverter converter) {
+                                                            JwtReactiveAuthenticationManager authManager) {
         AuthenticationWebFilter jwtFilter = new AuthenticationWebFilter(authManager);
-        jwtFilter.setServerAuthenticationConverter(converter);
+
+        jwtFilter.setServerAuthenticationConverter(new ServerBearerTokenAuthenticationConverter());
 
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
                 .authorizeExchange(exchanges -> exchanges
                         .pathMatchers("/actuator/health").permitAll()
                         .pathMatchers("/auth/**", "/usuarios/**").permitAll()
@@ -32,6 +34,7 @@ public class SecurityConfig {
                 .build();
     }
 }
+
 /* Define quais sao as rotas publicas e as autenticadas
 Registra o AuthenticationWebFilter, que vai chaar o converter e o manager para validar o jwt
  */
