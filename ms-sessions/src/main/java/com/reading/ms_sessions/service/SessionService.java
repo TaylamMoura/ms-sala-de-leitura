b1ca54eac9b7b54065ed3e796edda50ec04088d3
Taylam Moura <tay.moura@hotmail.com>
2026-02-11 18:44:01 -0300

    fix(session): ajustar SessionService para remover uso de CatalogClient e token

--- ms-sessions/src/main/java/com/reading/ms_sessions/service/SessionService.java
+++ ms-sessions/src/main/java/com/reading/ms_sessions/service/SessionService.java
@@ -1,6 +1,5 @@
 package com.reading.ms_sessions.service;
 
-import com.reading.ms_sessions.dto.BookDTO;
 import com.reading.ms_sessions.entity.ReadingSession;
 import com.reading.ms_sessions.repository.SessionsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,54 +12,36 @@ import java.time.LocalDateTime;
 public class SessionService {
 
     private final SessionsRepository sessionsRepository;
-    private final CatalogClient catalogClient;
 
-
-    //Instância da entidade SessaoService
     @Autowired
-    public SessionService(SessionsRepository sessionsRepository, CatalogClient catalogClient) {
+    public SessionService(SessionsRepository sessionsRepository) {
         this.sessionsRepository = sessionsRepository;
-        this.catalogClient = catalogClient;
     }
 
-    // Funções do Cronômetro
-    public ReadingSession startSession(Long userId, Long bookId, String token) {
-        //Validar livro no ms-catalog
-        BookDTO bookDTO = catalogClient.getBook(bookId, token);
-
-        // Salva sessão
+    // Iniciar Sessão
+    public ReadingSession startSession(Long userId, Long bookId) {
         ReadingSession session = new ReadingSession();
         session.setUserId(userId);
         session.setBookId(bookId);
         session.setStartPage(getLastReadPage(bookId));
-        session.setCoverUrl(bookDTO.coverUrl());
         session.setStartTime(LocalDateTime.now());
 
         return sessionsRepository.save(session);
     }
 
-
     // Finalizar Sessão
     @Transactional
-    public ReadingSession endSession(Long userId, Long bookId, int endPage, int readingTimeInSeconds, String token) {
+    public ReadingSession endSession(Long userId, Long bookId, int endPage, int readingTimeInSeconds) {
         int lastPage = getLastReadPage(bookId);
 
         ReadingSession session = new ReadingSession();
         session.setUserId(userId);
         session.setBookId(bookId);
-        session.setStartPage(lastPage); //página inicial da última sessão salva
-        session.setEndPage(endPage);    // página em que usuário parou nesta sessão
+        session.setStartPage(lastPage);
+        session.setEndPage(endPage);
         session.setReadingTime(readingTimeInSeconds);
         session.setStartTime(LocalDateTime.now());
         session.setEndTime(session.getStartTime().plusSeconds(readingTimeInSeconds));
-
-        // Valida livro no catálogo
-        BookDTO bookDTO = catalogClient.getBook(bookId, token);
-
-        //Marcar livro como 'finalizado' no ms-catalog se chegar a última página
-        if (endPage >= bookDTO.pages()){
-            catalogClient.markBookAsFinished(bookId, token);
-        }
 
         return sessionsRepository.save(session);
     }
