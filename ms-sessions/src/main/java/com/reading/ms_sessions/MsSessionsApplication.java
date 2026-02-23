40b1a66043ed017c23707ecfb12db1f354b9d4c4
Taylam Moura <tay.moura@hotmail.com>
2026-02-23 17:36:03 -0300

    feat(session):  habilita Feign Clients em MsSessionsApplication com @EnableFeignClients

--- ms-sessions/src/main/java/com/reading/ms_sessions/MsSessionsApplication.java
+++ ms-sessions/src/main/java/com/reading/ms_sessions/MsSessionsApplication.java
@@ -2,8 +2,10 @@ package com.reading.ms_sessions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MsSessionsApplication {
 
 	public static void main(String[] args) {
